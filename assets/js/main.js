14a54a1d5d4d568ae7ab925c54196831767121b3
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-08 22:47:35 +0400

    accardion added too

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,3 +115,36 @@ function playVideo(file) {
   myVideo.scr = file;
   videoPlayer.style.display = "block";
 }
+//accardion
+const accardionContent = document.querySelectorAll(".accardion-content");
+accardionContent.forEach((item, index) => {
+  let header1 = item.querySelector(".header1");
+  header1.addEventListener("click", () => {
+    item.classList.toggle("open");
+    let description = item.querySelector(".description");
+    console.log(item);
+    console.log(description.scrollHeight);
+    console.log(description);
+    if (item.classList.contains("open")) {
+      description.style.height = `${description.scrollHeight}px`;
+      item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+      console.log("hh");
+    } else {
+      description.style.height = "0px";
+      item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+      console.log("nn");
+    }
+    removeOpen(index);
+  });
+});
+
+function removeOpen(index1) {
+  accardionContent.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("open");
+      let des = item2.querySelector(".description");
+      des.style.height = "0px";
+      item2.querySelector("i").classList.replace("fa-minus","fa-plus")
+    }
+  });
+}
