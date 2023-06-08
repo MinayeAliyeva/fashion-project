527246c4290fda6faafea917bdf585d7f419a592
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-08 22:23:29 +0400

    accardion added

---
+++ assets/js/kids.js
@@ -0,0 +1,52 @@
+let meniIcon = document.querySelector("#menuIcon");
+let nav = document.querySelector("nav");
+console.log(nav);
+meniIcon.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  meniIcon.classList.contains("fa-bars")
+    ? (meniIcon.classList = "fa-solid fa-xmark")
+    : (meniIcon.classList = "fa-solid fa-bars");
+});
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.style.backgroundColor = "white";
+  header.style.transition = "0.5s";
+  if (scrollY == 0) {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+//accardion
+const accardionContent = document.querySelectorAll(".accardion-content");
+accardionContent.forEach((item, index) => {
+  let header1 = item.querySelector(".header1");
+  header1.addEventListener("click", () => {
+    item.classList.toggle("open");
+    let description = item.querySelector(".description");
+    console.log(item);
+    console.log(description.scrollHeight);
+    console.log(description);
+    if (item.classList.contains("open")) {
+      description.style.height = `${description.scrollHeight}px`;
+      item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+      console.log("hh");
+    } else {
+      description.style.height = "0px";
+      item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+      console.log("nn");
+    }
+    removeOpen(index);
+  });
+});
+
+function removeOpen(index1) {
+  accardionContent.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("open");
+      let des = item2.querySelector(".description");
+      des.style.height = "0px";
+      item2.querySelector("i").classList.replace("fa-minus","fa-plus")
+    }
+  });
+}
