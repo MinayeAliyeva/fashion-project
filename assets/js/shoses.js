844edfb6855761e27d7b532eccd63a2fb062dfce
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-09 22:15:30 +0400

    add actions

--- assets/js/shoses.js
+++ assets/js/shoses.js
@@ -6,3 +6,12 @@ window.addEventListener("scroll", () => {
     header.style.backgroundColor = "transparent";
   }
 });
+//
+document.getElementById("next").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").appendChild(lists[0]);
+};
+document.getElementById("prev").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").prepend(lists[lists.length-1]);
+};
