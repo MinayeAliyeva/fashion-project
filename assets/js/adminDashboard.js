fcf420a428eada0177c616c3e63e8c1206ab4724
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-13 10:45:03 +0400

    some style changes

--- assets/js/adminDashboard.js
+++ assets/js/adminDashboard.js
@@ -1,77 +1,22 @@
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-// //new trendings
-// const NEW_TRENDINGS = "http://localhost:3000/newtrending-products";
-// let addProductBtn2 = document.querySelector(".addProductBtn2");
-// addProductBtn2.addEventListener("click", () => {
-//   window.location = "add-edit-new-products-form.html";
-// });
-
-// let searchInput2 = document.querySelector(".searchInput2");
-// let arrCopy2 = [];
-// let filteredData2 = [];
-// let num2 = 10;
-// let mainRow2 = document.querySelector(".mainRow2");
-// async function fillProducts2() {
-//   let res = await axios(NEW_TRENDINGS);
-//   let data = await res.data;
-//   arrCopy2 = data;
-//   filteredData2 = filteredData2.length
-//     ? filteredData2.slice(0, num2)
-//     : data.slice(0, num2);
-//   mainRow2.innerHTML = "";
-//   filteredData2.forEach((obj) => {
-//     mainRow2.innerHTML += `
-//     <div class="col col-lg-1 ">
-// <div class="card">
-//   <div class="img">
-//     <img src="${obj.img}" alt="" />
-//     <div class="text-side">
-//       <span>${obj.productName}</span>
-//       <span>Price:${obj.productprice}</span>
-//     </div>
-//   </div>
-//   <div class="actions">
-//     <i class="fa-solid fa-trash" onclick=delFun2(${obj.id})></i>
-//  <a href="add-edit-new-products-form.html?id=${obj.id}">   <i class="fa-solid fa-pen" ></i></a>
-//     <i class="fa-solid fa-eye"></i>
-//   </div>
-// </div>
-// </div>
-
-// `;
-//   });
-// }
-// fillProducts2();
-
-// //delete
-// async function delFun2(id) {
-//   await axios.delete(`${NEW_TRENDINGS}/${id}`);
-//   filteredData = arrCopy
-//     .filter((obj) => {
-//       obj.id != id;
-//     })
-//     .slice(0, num);
-// }
-// //search
-// searchInput2.addEventListener("input", (e) => {
-//   filteredData2 = arrCopy2
-//     .filter((obj) => {
-//       return obj.productName
-//         .toLocaleLowerCase()
-//         .includes(e.target.value.toLocaleLowerCase());
-//     })
-//     .slice(0, num);
-
-//   fillProducts2();
-// });
-// //sort
-// let select = document.querySelector("#select");
-// select.addEventListener("change", (e) => {
-//   if (e.target.value == "from cheap to expensive") {
-//     filteredData = filteredData.sort((a, b) => a.productprice - b.productprice);
-//     fillProducts();
-//   } else if (e.target.value == "from expensive to cheap") {
-//     filteredData = filteredData.sort((a, b) => b.productprice - a.productprice);
-//     fillProducts();
-//   }
-// });
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
