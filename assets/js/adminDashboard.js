023e7568e81669f98ec02e698e4210a5f5443682
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-13 12:04:08 +0400

    added charts

--- assets/js/adminDashboard.js
+++ assets/js/adminDashboard.js
@@ -1,6 +1,5 @@
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
-
+const xValues = ["January","Fabruary","Marth","April","May","June","July","August","September","October","Novomber","December"];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15,60];
 new Chart("myChart", {
   type: "line",
   data: {
@@ -17,6 +16,34 @@ new Chart("myChart", {
     legend: {display: false},
     scales: {
       yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
+
+//
+var xValues2 = ["Dress", "Shoses", "Make up", "Men", "Kids"];
+var yValues2 = [90, 49, 84, 94, 75];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart2", {
+  type: "doughnut",
+  data: {
+    labels: xValues2,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues2
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Wie"
     }
   }
 });
