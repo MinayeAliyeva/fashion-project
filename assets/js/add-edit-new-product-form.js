d827783695cd68ea58e320f843da2a1354c783b1
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-13 10:24:37 +0400

    added new products

--- assets/js/add-edit-new-product-form.js
+++ assets/js/add-edit-new-product-form.js
@@ -1,12 +1,13 @@
-const NEW_TRENDINGS = "http://localhost:3000/newtrending-products";
+const NEWPRODUCTS_URL = "http://localhost:3000/newtrending-products";
 let form2 = document.querySelector("#form2");
 let productName2 = document.querySelector("#productName2");
 let productPrice2 = document.querySelector("#productPrice2");
 let imgInput2 = document.querySelector("#imgInput2");
+let base64;
 //edit
 let id = new URLSearchParams(window.location.search).get("id");
 async function fillInputsValue() {
-  let res = await axios(`${NEW_TRENDINGS}/${id}`);
+  let res = await axios(`${NEWPRODUCTS_URL}/${id}`);
   let data = await res.data;
   productName2.value = data.productName;
   productPrice2.value = data.productprice;
@@ -17,13 +18,33 @@ form2.addEventListener("submit", async (e) => {
   let productObj = {
     productName: productName2.value,
     productprice: productPrice2.value,
-    img: `./assets/products-image/${imgInput2.value.split("\\")[2]}`,
+    img: base64,
   };
   if (id) {
-    await axios.patch(`${NEW_TRENDINGS}/${id}`, productObj);
-    window.location = "adminDashboard.html";
+    await axios.patch(`${NEWPRODUCTS_URL}/${id}`, productObj);
+    window.location = "new-admin-products.html";
   } else {
-    await axios.post(`${NEW_TRENDINGS}`, productObj);
-    window.location = "adminDashboard.html";
+    await axios.post(`${NEWPRODUCTS_URL}`, productObj);
+    window.location = "new-admin-products.html";
   }
+});
+
+const convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
+const uploadImg = async (e) => {
+  const file = e.target.files[0];
+  base64 = await convertBase64(file);
+};
+imgInput2.addEventListener("change", (e) => {
+  uploadImg(e);
 });
