c204a3a11cad2fdb7d3f7abcb3a463668657794a
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-15 19:27:51 +0400

    added funciyality

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,11 +1,16 @@
 const BASE_URL = "http://localhost:3000/users";
-
 let row = document.querySelector(".row");
+let searchInput2 = document.querySelector(".searchInput2");
+let arrCopy = [];
+let filteredData = [];
+let num = 10;
 async function fillCard() {
-  let res = await axios(`${BASE_URL}`);
+  let res = await axios(BASE_URL);
   let data = await res.data;
-  console.log(data);
-  data.forEach((obj) => {
+  arrCopy = data;
+  filteredData = filteredData.length ? filteredData : data;
+  row.innerHTML = "";
+  filteredData.forEach((obj) => {
     row.innerHTML += `
     <div class="col-lg-3">
     <div class="card">
@@ -17,16 +22,24 @@ async function fillCard() {
       </div>
     </div>
   </div> 
-    `;
+        `;
   });
 }
-
 fillCard();
+//delete
 async function delFun(id) {
   await axios.delete(`${BASE_URL}/${id}`);
-  let res = await axios(`${BASE_URL}`);
-  let data = await res.data;
-  data.filter((obj) => {
+  filteredData = arrCopy.filter((obj) => {
     obj.id != id;
   });
 }
+//
+searchInput2.addEventListener("input", (e) => {
+  filteredData = arrCopy.filter((obj) => {
+    return obj.userName
+      .toLocaleLowerCase()
+      .includes(e.target.value.toLocaleLowerCase());
+  });
+
+  fillCard();
+});
