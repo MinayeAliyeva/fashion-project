cb1209d977f46c37bcb32ea60687a5192f7e4331
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-20 15:47:55 +0400

    shoses make dinamically

---
+++ assets/js/shose-add-edit.js
@@ -0,0 +1,50 @@
+const SHOSES_URL = "http://localhost:3000/shoses";
+let form5 = document.querySelector("#form5");
+let productName5 = document.querySelector("#productName5");
+let productPrice5 = document.querySelector("#productPrice5");
+let imgInput5 = document.querySelector("#imgInput5");
+let base64;
+//edit
+let id = new URLSearchParams(window.location.search).get("id");
+async function fillInputsValue() {
+  let res = await axios(`${SHOSES_URL}/${id}`);
+  let data = await res.data;
+  productName5.value = data.productName;
+  productPrice5.value = data.productprice;
+}
+fillInputsValue();
+form5.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let productObj = {
+    productName: productName5.value,
+    productprice: productPrice5.value,
+    img: base64,
+  };
+  if (id) {
+    await axios.patch(`${SHOSES_URL}/${id}`, productObj);
+    window.location = "shoseDash.html";
+  } else {
+    await axios.post(`${SHOSES_URL}`, productObj);
+    window.location = "shoseDash.html";
+  }
+});
+
+const convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
+const uploadImg = async (e) => {
+  const file = e.target.files[0];
+  base64 = await convertBase64(file);
+};
+imgInput5.addEventListener("change", (e) => {
+  uploadImg(e);
+});
