3c7da91602fd44cbf6e94e6d60496120b06e1fba
MinayeAliyeva <7s346pu@code.edu.az>
2023-06-20 09:48:17 +0400

    scrolls fixed

--- assets/js/makeup.js
+++ assets/js/makeup.js
@@ -1,12 +1,4 @@
-//menubar
-let meniIcon = document.querySelector("#menuIcon");
-let nav = document.querySelector("nav");
-meniIcon.addEventListener("click", () => {
-  nav.classList.toggle("show");
-  meniIcon.classList.contains("fa-bars")
-    ? (meniIcon.classList = "fa-solid fa-xmark")
-    : (meniIcon.classList = "fa-solid fa-bars");
-});
+
 //header
 let header = document.querySelector("header");
 window.addEventListener("scroll", () => {
